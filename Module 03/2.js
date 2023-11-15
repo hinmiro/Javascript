33ffc6d56452d6e9f7e69f5b2dd06a20b82d70cc
mirohi <m_hintikka@hotmail.com>
2023-11-15 12:45:13 +0200

    init

--- Module 03/2.js
+++ Module 03/2.js
@@ -1,9 +1,19 @@
 'use strict';
 
+
 const ul = document.querySelector('#target');
-const li = document.createElement('li');
+let li = document.createElement('li');
+li.appendChild(document.createTextNode('First item'));
+ul.appendChild(li);
 
+let li1 = document.createElement('li');
+li1.appendChild(document.createTextNode('Second item'));
+ul.appendChild(li1);
 
+let li2 = document.createElement('li');
+li2.appendChild(document.createTextNode('Third item'));
+ul.appendChild(li2);
 
-const element1 = document.getElementsByTagName('li')[1];
-element1.classList.add('my-list');
+const element = document.querySelector('#target');
+const elementChild = element.getElementsByTagName('li')[1];
+elementChild.classList.add('my-item');
