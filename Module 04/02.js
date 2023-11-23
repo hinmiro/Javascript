6af357698582097806e11ed3eb09eb1d4e2c1416
mirohi <m_hintikka@hotmail.com>
2023-11-23 14:49:17 +0200

    init

---
+++ Module 04/02.js
@@ -0,0 +1,14 @@
+'use strict';
+const searchQuery = document.querySelector('#query');
+const button = document.querySelector('input[type="submit"]');
+
+button.addEventListener('click', async function (evt) {
+    evt.preventDefault();
+    const code = searchQuery.value;
+    try {
+        const response = await fetch(`https://api.tvmaze.com/search/shows?q=${code}`)
+        console.log(response);
+    } catch (error) {
+        console.log(error.message);
+    }
+})
