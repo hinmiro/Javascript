2beba806706d01e5fc26710ccb264cab3c649b3b
mirohi <m_hintikka@hotmail.com>
2023-11-24 16:32:14 +0200

    init

---
+++ Module 04/03.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const search = document.querySelector('#query');
+const button = document.querySelector('input[type="Submit"]');
+const target = document.querySelector('#target');
+
+button.addEventListener('click', async function(evt) {
+    evt.preventDefault();
+    const query = search.value;
+    const data = await getData(query);
+    console.log(data);
+    console.log(data[0]['show']['name'])
+    target.innerHTML = '';
+    postData(data);
+
+})
+
+async function getData(query){
+    const url = `https://api.tvmaze.com/search/shows?q=${query}`;
+    const response = await fetch(url);
+    return await response.json();
+}
+
+function postData(data) {
+    for (let byte of data) {
+        const article = document.createElement('article');
+        const h2 = document.createElement('h2');
+        const url = document.createElement('a');
+        const img = document.createElement('img');
+        const summary = document.createElement('div');
+        h2.append(document.createTextNode(byte.show.name));
+        article.appendChild(h2);
+        img.src = byte.show.image?.medium;
+        img.alt = '';
+        article.appendChild(img);
+        url.href = byte.show.url;
+        url.textContent = byte.show.name;
+        url.target = '_blank';
+        article.appendChild(url);
+        summary.innerHTML = byte.show.summary;
+        article.appendChild(summary);
+        target.appendChild(article);
+
+    }
+}
